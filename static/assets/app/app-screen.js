1d81d372c177b5fc3b5b37ec484c7d6e8d86dc1a
wdchang <13changice@tongji.edu.cn>
2017-09-18 10:53:08 +0800

    init

--- static/assets/app/app-screen.js
+++
@@ -1,5 +0,0 @@
-/**
- * Created by Administrator on 2017/5/24 0024.
- */
-'use strict';
-var _myScreen= angular.module('myScreen',[]);
--- static/assets/app/controllers/basicsCtrl.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by Administrator on 2017/5/24 0024.
- */
-'use strict';
-_myScreen.controller('basicsCtrl',function($scope){
-    $scope.text='1900';
-});
